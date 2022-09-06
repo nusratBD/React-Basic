9005d60688b1075e4ee8c9ee2d63160dcc16bfdd
nusratBD <nusrat.hurain15@gmail.com>
2022-09-07 00:46:48 +0600

    4 types of web component

---
+++ 2.Web-Component/1.web-component-4-types.js
@@ -0,0 +1,7 @@
+/* 
+Component can identify in 4 ways:
+1. SImilar in Look, different in data
+2. A lot of sImilar in Look, different in data contains in a container
+3. No pattern but break down for working purpose
+4. Stand-alone component-suc as: header, footer
+*/
