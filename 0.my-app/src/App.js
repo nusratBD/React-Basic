58f330fbdc88d111fc675fd9ac331d273dfadc92
nusratBD <nusrat.hurain15@gmail.com>
2022-09-10 23:32:07 +0600

    JSX

--- 0.my-app/src/App.js
+++ 0.my-app/src/App.js
@@ -5,6 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>JSX</h1> 
+        {/* JSX stands for JavaScript XML.
+            JSX allows us to write HTML in React.
+            JSX makes it easier to write and add HTML in React. */}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
