33d0e52d26d0566e64712bdb5dd8df1afac9dfb9
nusratBD <nusrat.hurain15@gmail.com>
2022-09-11 22:33:32 +0600

    reserve-keywords

---
+++ 3.JSX/1.reserve-keywords.js
@@ -0,0 +1,8 @@
+/* 
+There are some reserve keywords in every programming language that we can not use accroding
+to our wish rather we must use them according to the strict rule of the language.
+
+Examplae: for, while, switch, if, var, let, const, function, class. We can not use them every where. We can only use them for the specific task.
+
+Similarly className is a reserve keyword of React.
+*/
