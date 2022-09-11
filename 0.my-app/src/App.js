447dac099bb38e574749a6e3319858ea2c84976a
nusratBD <nusrat.hurain15@gmail.com>
2022-09-11 23:43:40 +0600

    JSX

--- 0.my-app/src/App.js
+++ 0.my-app/src/App.js
@@ -1,6 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
+const x = 200;
+const self = {
+  name: 'Nusrat Hurain',
+  age: 25,
+  profession: 'Software Engineer'
+}
+const testStyle = {
+    color: 'white',
+    fontWeight: 200
+}
 function App() {
   return (
     <div className="App">
@@ -10,6 +19,20 @@ function App() {
             JSX allows us to write HTML in React.
             JSX makes it easier to write and add HTML in React. */}
         <img src={logo} className="App-logo" alt="logo" />
+        <div className="container">
+          <h3>Hey, Nusrat. How are you?</h3>
+        </div>
+        <div className="test">
+          <h2>Benifits of JSX:</h2>
+          <p>1. Any Valid JS Expression can be used here.</p>
+          <p>Example: {50 + x}</p>
+          <p>2. Object Access: Details of '{self.name}'</p>
+          <p>Age: {self.age} Profession: {self.profession}</p>
+          <p>3. CSS Style</p>
+          <p style={testStyle}>Name: {self.name}</p>
+          <p style={testStyle}>This CSS style is from react JSX</p>
+          <p style={{color: 'red', fontSize: '20px', fontWeight:'bold'}}>4. Inline Style</p>
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ 2.JSX/2.why-jsx.js
@@ -0,0 +1,13 @@
+/* 
+It is very powerful:
+1. Syntax highlights here
+2. We can use emmet
+3. We can use variable directly(Actually we can write any valid JS expression here)
+*/
+
+/* 
+1. JSX=>Javascript XML
+2. Allows us to write HTML in React
+3. Writing and Adding HTML in React is easier
+# React converts this syntax into JS
+*/
