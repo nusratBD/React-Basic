18a8e5758228fa58cb236c64a516db1e1c39f279
nusratBD <nusrat.hurain15@gmail.com>
2022-09-13 01:37:40 +0600

    Create Component

--- 0.my-app/src/App.js
+++ 0.my-app/src/App.js
@@ -13,40 +13,26 @@ const testStyle = {
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>JSX</h1> 
-        {/* JSX stands for JavaScript XML.
-            JSX allows us to write HTML in React.
-            JSX makes it easier to write and add HTML in React. */}
-        <img src={logo} className="App-logo" alt="logo" />
-        <div className="container">
-          <h3>Hey, Nusrat. How are you?</h3>
-        </div>
-        <div className="test">
-          <h2>Benifits of JSX:</h2>
-          <p>1. Any Valid JS Expression can be used here.</p>
-          <p>Example: {50 + x}</p>
-          <p>2. Object Access: Details of '{self.name}'</p>
-          <p>Age: {self.age} Profession: {self.profession}</p>
-          <p>3. CSS Style</p>
-          <p style={testStyle}>Name: {self.name}</p>
-          <p style={testStyle}>This CSS style is from react JSX</p>
-          <p style={{color: 'red', fontSize: '20px', fontWeight:'bold'}}>4. Inline Style</p>
-        </div>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Similar in Look, Similar in Data */}
+      <Person></Person>
+      <Person></Person>
     </div>
   );
+}
+//Function is nothing but a component
+function Person(){
+  // return <div>
+  //   <h1>Hello, Nusrat</h1>
+  //   <p>Profession: Software Engineer</p>
+  // </div>
+
+  //Indentaion is understandable
+  return(
+    <div className="hello">
+      <h1>Hello, Nusrat</h1>
+      <p>Profession: Software Engineer</p>
+  </div>
+  )
 }
 
 export default App;
---
+++ 3.crate-component/1.component.js
@@ -0,0 +1,10 @@
+/* 
+We can return only one element in in JS function. If we want to return multiple this, we
+follow the below methods:
+1.Keep everything in a div. 
+*/
+
+/* 
+Reason to capitalize the componet:
+Because it makes a difference with html tag.
+*/
