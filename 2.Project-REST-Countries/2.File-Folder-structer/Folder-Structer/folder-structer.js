a815c78af06346f1e3d191788b0d4da45712b020
nusratBD <nusrat.hurain15@gmail.com>
2022-10-03 21:35:02 +0600

    File/Folder Structer

---
+++ 2.Project-REST-Countries/2.File-Folder-structer/Folder-Structer/folder-structer.js
@@ -0,0 +1,5 @@
+/* 
+When there are a lot of component in a project, it is very difficult to
+handle them in one page. In that case, we can create a seperate folder to
+keep all the component. This is called file or folder structer.
+*/
