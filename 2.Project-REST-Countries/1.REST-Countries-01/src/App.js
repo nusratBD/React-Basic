533a8b818f13757743e985f5189973d65fa62583
nusratBD <nusrat.hurain15@gmail.com>
2022-10-03 21:27:00 +0600

    REST-Countries-01

--- 2.Project-REST-Countries/1.REST-Countries-01/src/App.js
+++ 2.Project-REST-Countries/1.REST-Countries-01/src/App.js
@@ -23,6 +23,27 @@ function LoadCountries(){
       <h1>Visiting Every Countries of the World!!!</h1>
       {/* Step 04: Show loaded data */}
       <h3>Available Countries: {countries.length}</h3>
+
+
+      {/* 01: Show Single Data  */}
+      {/* {
+        countries.map(country=><p>{country.name.common}</p>)
+      } */}
+
+
+     {/*  02. Show multiple data by using props: step-02 */}
+     {
+      countries.map(country=><Country name={country.name.common} population={country.population}></Country>)
+     }
+    </div>
+  )
+}
+//02. Show multiple data by using props: step-01
+function Country(props){
+  return(
+    <div>
+      <h3 style={{color: 'green'}}>Name: <small style={{color: 'indigo'}}>{props.name}</small></h3>
+      <h3 style={{color: 'blue'}}>Population: <small style={{color: 'magenta'}}>{props.population}</small></h3>
     </div>
   )
 }
