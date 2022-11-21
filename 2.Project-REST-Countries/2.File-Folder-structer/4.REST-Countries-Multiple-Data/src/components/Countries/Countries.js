1de89481ba7b6e0c80347b43e2c1b2e75e589ab0
nusratBD <nusrat.hurain15@gmail.com>
2022-11-21 22:03:32 +0600

    grid css and unique key in react

--- 2.Project-REST-Countries/2.File-Folder-structer/4.REST-Countries-Multiple-Data/src/components/Countries/Countries.js
+++ 2.Project-REST-Countries/2.File-Folder-structer/4.REST-Countries-Multiple-Data/src/components/Countries/Countries.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Country from '../Country/Country';
+import './Countries.css';
 const Countries = () => {
     const [countries, setCountries] = useState([]);
     useEffect(()=>{
@@ -10,12 +11,14 @@ const Countries = () => {
     return (
         <div>
             <h1>Hello {countries.length} Countries All Around The World!!</h1>
-            {
+            {/* {
                 countries.map(country=>console.log(country))
-            }
+            } */}
+            <div className='countries-container'>
             {
-                countries.map(country=><Country country={country}></Country>)
+                countries.map(country=><Country country={country} key={country.cca3}></Country>)
             }
+            </div>
         </div>
     );
 };
--- 2.Project-REST-Countries/2.File-Folder-structer/4.REST-Countries-Multiple-Data/src/components/Country/Country.js
+++ 2.Project-REST-Countries/2.File-Folder-structer/4.REST-Countries-Multiple-Data/src/components/Country/Country.js
@@ -2,10 +2,16 @@ import React from 'react';
 import './Country.css'
 
 const Country = (props) => {
-   const {name, area, region} = props.country;
+   const {name, area, region, flags} = props.country;
     return (
         <div className='country'>
+            {/* {console.log(props.country)} */}
+            {/*We get an error in the browser:
+            react-jsx-dev-runtime.development.js:87 Warning: Each child in a list should have a unique "key" prop.
+            Solution: We must use a key to solve this problem.
+            It improves the performance of react.*/}
             <h1>Name: {name.common}</h1>
+            <img src={flags.png}></img>
             <h2>Area: {area}</h2>
             <h3>Region: {region}</h3>
         </div>
